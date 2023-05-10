5d5c56cede281cca47107cb13528cb700d700ee4
Andrea Ramirez <andrearamirezfernandez@gmail.com>
2023-05-09 21:56:25 -0600

    adds pop-up de propina

--- Weiter/components/CuentaCliente.js
+++ Weiter/components/CuentaCliente.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import {StyleSheet, Text, View, Button, Pressable } from 'react-native';
 import OrderRow from './OrderRow';
+import ModalPropina from './ModalPropina';
 
 const CuentaCliente = props => {
+  const [isModalOpen, setIsModalOpen, porcentajePropina] = useState(false);
+
     return (
         <View style={[
           styles.container,
@@ -18,7 +21,7 @@ const CuentaCliente = props => {
               <Text style = {styles.smallText}>Mesero</Text>
             </View>
           </View>
-          <View style={{flex: 3, }}>
+          <View style={{flex: 1, }}>
             <OrderRow cantidad = {1} nombre = "Hamburguesa" costo = {150} />
             <OrderRow cantidad = {1} nombre = "Hamburguesa" costo = {150} />
             <OrderRow cantidad = {1} nombre = "Hamburguesa" costo = {150} />
@@ -33,15 +36,20 @@ const CuentaCliente = props => {
               <Text style={{flex: 1}} >IVA:</Text>
               <Text style={{flex: 1, textAlign: 'right'}} >$20</Text>
             </View>
+            <View style={{flex: 1, flexDirection: 'row', padding: 20}}>
+              <Text style={{flex: 1}} >Propina:</Text>
+              <Text style={{flex: 1, textAlign: 'right'}} >0</Text>
+            </View>
             <View style={{flex: 1, flexDirection: 'row', padding: 20,}}>
               <Text style={{flex: 1}} >Total:</Text>
               <Text style={{flex: 1,textAlign: 'right'}} >$1540</Text>
             </View>
           </View>
           <View style={{flex: 1, padding: 20, alignItems: 'center', justifyContent: 'center',}}>
-            <Pressable style = {styles.boton}>
+            <Pressable style = {styles.boton} onPress={() => setIsModalOpen(!isModalOpen)}>
               <Text style = {styles.smallText}>AGREGAR PROPINA</Text>
             </Pressable>
+            <ModalPropina isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} porcentajePropina={0}></ModalPropina>
             <Pressable style = {styles.boton}>
               <Text style = {styles.smallText}>PAGAR</Text>
             </Pressable>
