f85030df00f39a0a6985a272b84aca027b390791
Sofia Vega <sofia.vega18@gmail.com>
2023-06-12 14:32:49 -0600

    jalar informacion de mesas desde firebase

--- Weiter/components/Mesas.js
+++ Weiter/components/Mesas.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, StyleSheet, View, Text, Pressable } from 'react-native';
 import { Table, Row, Rows } from 'react-native-table-component';
 import { useNavigation } from "@react-navigation/native";
-import { ref, get, child } from 'firebase/database'
+import { ref, get, child, database } from 'firebase/database'
 import { firebaseDB } from '../firebaseConfig';
 
 const styles = StyleSheet.create({
@@ -30,34 +30,79 @@ const styles = StyleSheet.create({
 const Mesas = () => {
   const navigation = useNavigation();
   const [active, setActive] = useState(false);
+  const [tableIds, setTableIds] = useState([])
+  const [tableData, setTableData] = useState({
+    tableHead: ['No.', 'Estado', 'Acción'],
+    tableData: [],
+  });
   const handleClick = () => {
     setActive(!active);
     setData(data);
     
   };
+  
+  const [rows, setRows] = useState([])
+  const onAbrirMesa = () => {
+    //pasar el id del children (row) para editar solo esa
 
-  var tableData = {
-    tableHead: ['No.', 'Estado', 'Acción'],
-    tableData: [
-        ['1', 'Abierta', <Button title="Editar" onPress={() => navigation.navigate('menuMesero')} color='#C8B8FF' visible={false}>Editar</Button>],
-        ['2', 'Pagada', <Button title="Eliminar" color='#F9553A'>Cerrar Mesa</Button>],
-        ['3', 'Cerrada', <Button onPress={handleClick} title="Abrir Mesa" color={active ? "black" : "#03ea60"}></Button>],
-    ],
+  }
+  const onEliminar = (row_id) => {
+    //pasar el id del children (row) para editar solo esa
+    // change table data to state and create new array to modify it
+    //var aux_arr = rows;
+    //aux_arr[row_id] = ['2', 'Cerrada', <Button onPress={handleClick} title="Abrir Mesa" color={active ? "black" : "#03ea60"}></Button>];
+    //setRows(aux_arr);
   };
 
-  const [data, setData] = useState(tableData);
-  // const starCountRef = ref(firebaseDB, 'mesa/mesaId');
+  //empezar todas como eliminar
+  //leer ids del realtime firebase
+
+  
+  useEffect(() => {
+    console.log("hola")
   console.log("aver ")
+
   // console.log(starCountRef)
-  get(child(ref(firebaseDB),'mesas/mesa01')).then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
+  console.log(tableIds)
+  console.log(rows)
+  if (tableData.tableData.length == 0){
+    setTableData({
+      tableHead: ['No.', 'Estado', 'Acción'],
+      tableData: rows,
+    })
+  }
+  if (tableIds.length == 0){
+    console.log("data from firebase")
+    setTableData({
+      tableHead: ['No.', 'Estado', 'Acción'],
+      tableData: rows,
+    })
+    get(child(ref(firebaseDB),'restaurante1/mesas')).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+        const prueba = snapshot.toJSON();
+        const ids = Object.keys(prueba)
+        console.log(prueba);
+        console.log(ids)
+        setTableIds(ids);
+        console.log(tableIds);
+        var new_rows = [];
+        for (let i = 0; i < ids.length; i++) {
+          console.log(ids[i]);
+          new_rows.push(
+            [ids[i], 'Cerrada', <Button onPress={handleClick} title="Abrir Mesa" color={active ? "black" : "#03ea60"}></Button>]);
+        }
+        setRows(new_rows);
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+    }, [tableIds, rows, tableData]);
+  
+  // const starCountRef = ref(firebaseDB, 'mesa/mesaId');
 
   return (
 
@@ -68,8 +113,8 @@ const Mesas = () => {
     </View>
       <View style={styles.container}>
           <Table>
-              <Row data={data.tableHead} style={styles.head} textStyle={styles.headText} />
-              <Rows data={data.tableData} textStyle={styles.text} />
+              <Row data={tableData.tableHead} style={styles.head} textStyle={styles.headText} />
+              <Rows data={tableData.tableData} textStyle={styles.text} />
           </Table>
       </View>
     </>
