04eed84dc63837a4b38d421f095e1ffbbf023480
María Renée <mariarbp@icloud.com>
2023-06-13 18:26:54 -0600

    Stripeishhh

--- Weiter/App.js
+++ Weiter/App.js
@@ -8,14 +8,24 @@ import MenuMesero from './components/MenuMesero';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ConfirmacionPagoCliente from './components/ConfirmacionPagoCliente';
+import StripeApp from "./src/StripeApp";
+import { StripeProvider } from "@stripe/stripe-react-native";
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
+      <StripeProvider publishableKey="pk_live_51NHcToHeDagzFAimyQJrSSjwsms6l8NMgSRkH3Be3TYlxdy5a5bJwXMTxjiQh8KZS6ryUovhcbIggEtSaMeESY6O00rpayVJej">
+      <StripeApp />
+      </StripeProvider>
+    
+  
       <NavigationContainer>
         <Stack.Navigator>
+
+
           {/* Mesero */}
           <Stack.Screen name="mesas" component={Mesas} />
           <Stack.Screen name="editarOrdenMesa" component={EditarOrdenMesa} />
